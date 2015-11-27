7dddb4c15474cff3dec916580a3e7090e4d37671
amg1893 <adrianmora784@gmail.com>
2015-11-27 17:26:26 -0500

    Anadida funcion para final del juego

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+$(function(){
+	$.scrollify({
+		section: '.panel',
+		offset: 0
+	});
+});
+
+function showAndPlay() {
+	$('div.video-popup').css('display', 'block');
+	$('div.video-popup').append('<iframe class="video-popup" src="https://www.youtube.com/embed/ZkJpzTNeaZQ?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+}
+
+function closeAndStop() {
+	$('div.video-popup').css('display', 'none');
+	$('iframe.video-popup').remove();
+}
+
+function endGame(punctuation,language) {
+	$('span.punctuation').text(punctuation);
+	/* Mostrar popups */
+}
