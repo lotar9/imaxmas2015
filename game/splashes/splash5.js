87d4c5521428298ec0cf6978a7cd181d83b5c14b
Alejandro Fernandez <alejandro.fernandez.rueda@gmail.com>
2015-11-28 17:43:21 +0100

    STFU

--- game/splashes/splash5.js
+++ game/splashes/splash5.js
@@ -2,7 +2,8 @@ var game = World.game;
 
 function preload()
 {
-    game.load.image( 'background','/sprites/Final/final.jpg' );
+    game.load.image( 'background','/game/sprites/level4/04_fondo.png' );
+    game.load.image( 'button','/game/sprites/shared/ok_button.gif' );
 }
 
 function create()
@@ -10,6 +11,14 @@ function create()
 	var background = game.add.tileSprite( 0, 0, game.width, game.height, 'background' );
     background.tileScale.y = 0.5;
    	background.tileScale.x = 0.5;
+
+    var button = game.add.button( 
+        game.world.centerX - game.cache.getImage( "button" ).width / 2, 
+        game.world.centerY - game.cache.getImage( "button" ).height / 2 + 150, 
+        'button', 
+        top.endGame.bind( this, World.totalScore ), 
+        this 
+    );
 
    	var text = game.add.text( 
         0, 
@@ -26,6 +35,10 @@ function create()
         { fontSize: '42px', fill: '#FFF', stroke: '#000', strokeThickness: '5' } 
     );
     text2.x = game.world.centerX - text2.width / 2;
+
+    var enter = game.input.keyboard.addKey( Phaser.Keyboard.ENTER );
+    enter.onDown.add( top.endGame.bind( this, World.totalScore ) );
 }
 
 World.addState( 'Splash5', { preload: preload, create: create } );
+World.goToLevel( 'Splash5' );
